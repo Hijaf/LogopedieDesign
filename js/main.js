3f40c546f95d284deee11eccd3abba0e94f07268
Hezekyel <micael.dekleyn@gmail.com>
2013-01-22 02:57:40 +0100

    Content Done (Except "Recherche") && Portraits Design - Done && Enseignements Design - Done && Agenda Design - Done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+/* Logopedie - Projet Web
+ * JS Document - /html5/test_two/js/script.js
+ * coded by Micael Dekleyn 2383
+ * 21 January 2012
+ */
 
+ ( function ( $ ) {
+	"use strict";
+
+	// --- global vars
+	var timer,
+		delay = 8000;
+
+	// --- methods
+	var changeImage = function(){
+		$( ".eventSlider" ).slideUp( 1000, function(){
+			$( ".eventSlider" ).slideDown( 800 );
+		} );
+	};
+
+	// --- onload
+	$( function () {
+		// --- onload routines
+		timer = setInterval(changeImage,delay);
+	} );
+
+}( jQuery ) );
---
+++ js/main_min.js
@@ -0,0 +1,5 @@
+/* Logopedie - Projet Web
+ * JS Document - /html5/test_two/js/script.js
+ * coded by Micael Dekleyn 2383
+ * 21 January 2012
+ */(function(e){"use strict";var t,n=8e3,r=function(){e(".eventSlider").slideUp(1e3,function(){e(".eventSlider").slideDown(800)})};e(function(){t=setInterval(r,n)})})(jQuery);
