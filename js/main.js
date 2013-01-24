990b8e89a5778835182712a78268c9d730ab0d5f
Hezekyel <micael.dekleyn@gmail.com>
2013-01-24 15:58:27 +0100

    MediaQueries Ipad - Done

--- js/main.js
+++ js/main.js
@@ -9,19 +9,89 @@
 
 	// --- global vars
 	var timer,
-		delay = 8000;
+		delay = 8000,
+		gmap,
+		styledMap;
+
+	var firstSlide = $( "#slider .eventSlider:first" ).attr( "id" ),
+		lastSlide = $( "#slider .eventSlider:last" ).attr( "id" );
 
 	// --- methods
+	// -- Slider Image
 	var changeImage = function(){
-		$( ".eventSlider" ).slideUp( 1000, function(){
-			$( ".eventSlider" ).slideDown( 800 );
+		var currentSlide = $( ".eventSlider:visible" ).attr( "id" );
+		var nextSlide = $( "#" + currentSlide ).next().attr( "id" );
+
+		nextSlide = currentSlide === lastSlide ? firstSlide : nextSlide ;
+
+		$( "#" + currentSlide ).slideUp( 1500, function(){
+			$( "#" + nextSlide ).slideDown( 2000 );
 		} );
-	};
+	}; // -- changeImage
+
+	// -- Generate map
+	var generateMap = function(){
+		var styles = [
+		    {
+		    	stylers: [
+		        	{ hue: "#001aff" },
+		        	{ saturation: -100 }
+		      	]	
+		    },{
+		      featureType: "road",
+		      elementType: "geometry",
+		      stylers: [
+		        { lightness: 100 },
+		        { visibility: "simplified" }
+		      ]
+		    },{
+		      featureType: "road",
+		      elementType: "labels",
+		      stylers: [
+		        { visibility: "on" }
+		      ]
+		    }
+		];
+
+		styledMap = new google.maps.StyledMapType(styles,
+	    				{name: "Styled Map"}
+	    			);
+
+		gmap = new google.maps.Map(document.getElementById("gmap"),{
+			zoom: 15,
+			mapTypeId: google.maps.MapTypeId.HYBRID,
+			center: new google.maps.LatLng( 50.5875838, 5.5702195),
+			mapTypeControlOptions: {
+		      mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+		    }
+		});
+
+		gmap.mapTypes.set('map_style', styledMap);
+	  	gmap.setMapTypeId('map_style');
+	}; // -- generateMap
+
+	var addMarker = function(){
+		var markerIcon = new google.maps.MarkerImage( "../img/marker.png" );
+		gmap.setZoom(12);
+		gmap.panTo(new google.maps.LatLng( 50.5875838, 5.5702195));
+		var marker = new google.maps.Marker({
+			map: gmap,
+			position: new google.maps.LatLng( 50.5875838, 5.5702195),
+			title: "Site universitaire du Sart Tilman, B38 Rue de l’Aunaie, 30 4000 Sart Tilman",
+			icon: markerIcon
+		});
+	}; // -- addMarker
 
 	// --- onload
 	$( function () {
 		// --- onload routines
-		timer = setInterval(changeImage,delay);
+		$( ".eventSlider" ).not( ":first" ).hide();
+		timer = setInterval( changeImage,delay );
+
+		if( $( "#gmap" ).length ){
+			generateMap();
+			addMarker();
+		}
 	} );
 
 }( jQuery ) );
