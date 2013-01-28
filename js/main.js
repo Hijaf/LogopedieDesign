2735f9eecbc502b013cfc466f5585e7d5b29d495
Hezekyel <micael.dekleyn@gmail.com>
2013-01-28 01:12:01 +0100

    Details Review

--- js/main.js
+++ js/main.js
@@ -72,14 +72,84 @@
 
 	var addMarker = function(){
 		var markerIcon = new google.maps.MarkerImage( "../img/marker.png" );
-		gmap.setZoom(12);
 		gmap.panTo(new google.maps.LatLng( 50.5875838, 5.5702195));
-		var marker = new google.maps.Marker({
-			map: gmap,
-			position: new google.maps.LatLng( 50.5875838, 5.5702195),
-			title: "Site universitaire du Sart Tilman, B38 Rue de l’Aunaie, 30 4000 Sart Tilman",
-			icon: markerIcon
-		});
+		if( $( "#gmap" ).attr( "class" ) !== "recherche" ){
+			gmap.setZoom(12);
+			var marker = new google.maps.Marker({
+				map: gmap,
+				position: new google.maps.LatLng( 50.5875838, 5.5702195),
+				title: "Site universitaire du Sart Tilman, B38 Rue de l’Aunaie, 30 4000 Sart Tilman",
+				icon: markerIcon
+			});
+		}else{
+			gmap.setZoom(5);
+			var markerMontreal = new google.maps.Marker({
+				map: gmap,
+				position: new google.maps.LatLng(45.500,-73.600),
+				title: "Montréal",			
+				icon: markerIcon
+			});
+			var markerPhiladelphie = new google.maps.Marker({
+				map: gmap,
+				position: new google.maps.LatLng(40.000,-75.167),
+				title: "Philadelphie",
+				icon: markerIcon
+			});		
+			var markerLiege = new google.maps.Marker({
+				map: gmap,
+				position: new google.maps.LatLng(50.5875838,5.5702195),
+				title: "Liège",
+				icon: markerIcon
+			});
+			var markerGand = new google.maps.Marker({
+				map: gmap,
+				position: new google.maps.LatLng(51.033,3.700),
+				title: "Gand",
+				icon: markerIcon
+			});
+			var markerBruxelles = new google.maps.Marker({
+				map: gmap,
+				position: new google.maps.LatLng(50.850,4.350),
+				title: "Bruxelles",
+				icon: markerIcon
+			});
+			var markerAnvers = new google.maps.Marker({
+				map: gmap,
+				position: new google.maps.LatLng(51.217,4.417),
+				title: "Anvers",
+				icon: markerIcon
+			});
+			var markerToulouse = new google.maps.Marker({
+				map: gmap,
+				position: new google.maps.LatLng(43.617,1.450),
+				title: "Toulouse",
+				icon: markerIcon
+			});
+			var markerMarseille = new google.maps.Marker({
+				map: gmap,
+				position: new google.maps.LatLng(43.300,5.367),
+				title: "Marseille",
+				icon: markerIcon
+			});
+			var markerLyon = new google.maps.Marker({
+				map: gmap,
+				position: new google.maps.LatLng(45.767,4.833),
+				title: "Lyon",
+				icon: markerIcon
+			});
+			var markerLondres = new google.maps.Marker({
+				map: gmap,
+				position: new google.maps.LatLng(51.500,-0.167),
+				title: "Londres",
+				icon: markerIcon
+			});
+			var markerNeuss = new google.maps.Marker({
+				map: gmap,
+				position: new google.maps.LatLng(51.200,6.6833),
+				title: "Neuss (Rhénanie du Nord Westphalie)",
+				icon: markerIcon
+			});	
+		}
 	}; // -- addMarker
 
 	// --- onload
